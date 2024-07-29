a2e8cd943e326fe5aefdcef4f0c927198ec276ad
Equipo <Equipo@DESKTOP-EAD5GPH>
2024-07-28 23:28:43 -0600

    Agregar archivos de proyecto.

---
+++ WindowsApp1/Module1.vb
@@ -0,0 +1,15 @@
+﻿Module Modulel
+
+    Sub Main()
+        Dim mensaje, nombre As String
+        Console.Clear()
+        Console.Write("Ingrese nombre y #Carnet: ")
+
+        nombre = Console.ReadLine()
+        Console.Clear()
+        mensaje = "Hola mundo, !!!! "
+        Console.WriteLine(mensaje + nombre + " 2024 !!!! ")
+        Console.Write("Presione cualquier tecla para salir")
+        Console.ReadKey()
+    End Sub
+End Module
